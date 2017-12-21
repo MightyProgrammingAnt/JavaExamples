f62c68ea59bd1f99e26d7ae9f26d1a0ffc67cb9b
sahni <sahnicoe@yahoo.com>
2017-12-20 19:57:33 -0800

    intial commit

---
+++ Documents/NetBeansProjects/JavaApplication12/src/javaapplication12/JavaApplication12.java
@@ -0,0 +1,24 @@
+package javaapplication12;
+
+/**
+ *
+ * @author sahni
+ */
+public class JavaApplication12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String word = "Hello";
+        String rev = " ";
+        int length = word.length();
+        
+        for (int i = length - 1; i >= 0; i--)
+        {
+            rev = rev + word.charAt(i);
+        }
+        System.out.print(rev);
+    }
+    
+}
