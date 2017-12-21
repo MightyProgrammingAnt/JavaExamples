610843e71a5a8a598064002465867039f7af121d
sahni <sahnicoe@yahoo.com>
2017-12-21 08:07:58 -0800

    intial commit

---
+++ Documents/NetBeansProjects/InterfaceExample1/src/InterfaceExample1/Resizable.java
@@ -0,0 +1,20 @@
+package InterfaceExample1;
+
+/// Interface Example
+/// File Name:     InterfaceExample1.cs
+/// @author:       Sahni Coe
+/// Date:          Dec. 12, 2017
+///
+/// Problem Statement: (simple interface example)
+///    
+/// Overall Plan (step-by-step, how you want the code to make it happen):
+/// 1.define an interface named resizable with just one abstract method, resize
+///   that is void with no parameter
+///
+/// Classes needed and Purpose (Input, Processing, Output)
+
+public interface Resizable {
+    
+    public abstract void resize();
+    
+}
